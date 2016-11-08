2f7249e2ad733946e9d146e7ef31e9ae9142d863
Tjeerd Verhagen <tjeerd.verhagen@duo.nl>
2016-11-08 13:21:03 +0100

    Adds some instances to the GasTycoon world

--- GasTycoon.java
+++ GasTycoon.java
@@ -16,6 +16,15 @@ public class GasTycoon extends World
     public GasTycoon()
     {    
         // Create a new world with 10x8 cells with a cell size of 32x32 pixels.
-        super(10, 8, 32); 
+        super(10, 8, 32);
+        addObject(new PipeStrait(), 0, 0);
+        addObject(new PipeStrait(), 1, 0);
+        addObject(new PipeStrait(), 2, 0);
+        addObject(new PipeElbow(), 0, 1);
+        addObject(new PipeElbow(), 1, 1);
+        addObject(new PipeElbow(), 2, 1);
+        addObject(new PipeTee(), 0, 2);
+        addObject(new PipeTee(), 1, 2);
+        addObject(new PipeTee(), 2, 2);
     }
 }
