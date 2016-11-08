6c33696b9969c9a43755a77fba9584cb565a1db0
vagrant <Tjeerd Verhagen>
2016-11-08 07:40:14 +0100

    initial files

---
+++ GasTycoon.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GasTycoon extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public GasTycoon()
+    {    
+        // Create a new world with 10x8 cells with a cell size of 30x30 pixels.
+        super(10, 8, 30); 
+    }
+}
