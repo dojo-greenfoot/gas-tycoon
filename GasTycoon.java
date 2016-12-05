118516226c31cc6463426f6facb4565ac02758cc
Tjeerd Verhagen <tjeerd.verhagen@gmail.com>
2016-12-05 12:06:34 +0100

    Adds implementation that show each actors position and rotation

--- GasTycoon.java
+++ GasTycoon.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Write a description of class MyWorld here.
@@ -17,6 +18,20 @@ public class GasTycoon extends World
     {    
         // Create a new world with 10x8 cells with a cell size of 32x32 pixels.
         super(10, 8, 32);
+        createContentSample2();
+    }
+
+    public void act() {
+        String key = Greenfoot.getKey();
+        if (key != null) {
+            System.out.println("key: " + key);
+            if (key.equals("s")) {
+                showActors();
+            }
+        }
+    }
+    
+    private void createContent() {
         addObject(new PipeStrait(), 0, 0);
         addObject(new PipeStrait(), 1, 0);
         addObject(new PipeStrait(), 2, 0);
@@ -27,4 +42,48 @@ public class GasTycoon extends World
         addObject(new PipeTee(), 1, 2);
         addObject(new PipeTee(), 2, 2);
     }
+
+    private void createContentSample1() {
+        addObject(new PipeStrait(), 1, 0);
+        Actor actor = getObjectsAt(1, 0, Pipe.class).get(0);
+        System.out.println("show current rotation: " + actor.getRotation());
+        actor.turn(90);
+        System.out.println("show current rotation: " + actor.getRotation());
+        
+        actor = new GasBurner();
+        actor.turn(90);
+        addObject(actor, 0, 0);
+        actor = new GasTank();
+        actor.turn(270);
+        addObject(actor, 2, 0);
+        
+    }
+    
+    public void createContentSample2() {
+        addObject(new PipeStrait(), 0, 1);
+        Actor actor = getObjectsAt(0, 1, Pipe.class).get(0);
+        show(actor);
+        
+        actor = new GasBurner();
+        actor.turn(180);
+        addObject(actor, 0, 0);
+        show(actor);
+
+        actor = new GasTank();
+        actor.turn(0);
+        addObject(actor, 0, 2);
+        show(actor);
+    }
+
+    private void showActors() {
+        List<Pipe> actors = getObjects(Pipe.class);
+        for (Actor actor : actors) {
+            show(actor);
+        }
+    }
+
+    public void show(Actor actor) {
+        System.out.println("Actor:" + actor.getClass().getName() + "  x:" + actor.getX() + "  y:" + actor.getY() + "  r:" + actor.getRotation());
+    }
+
 }
